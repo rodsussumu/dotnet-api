a5dca76b63dbbc8f8532e0af9ec0f5f895810a32
rodsussumu <rodsussumu@gmail.com>
2022-11-21 01:20:05 -0300

    add migrations, entities and repository

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            // Criar as migrations
+            var connectionString = "Server=localhost;Port=3306;Database=Course;Uid=root;Pwd=root";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
